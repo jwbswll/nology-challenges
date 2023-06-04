2a6bbc4e18f03791cc70e7acb7368d817d6a0353
jwbswll <boswelljackwilliam@gmail.com>
2023-06-05 09:45:11 +1000

    adds js types challenge

---
+++ js-challenges/types-challenge.js
@@ -0,0 +1,13 @@
+console.log(typeof 15); // Prediction: Number
+console.log(typeof 5.5); // Prediction: Number
+console.log(typeof NaN); // Prediction: Number
+console.log(typeof 'hello'); // Prediction: String
+console.log(typeof true); // Prediction: Boolean
+console.log(typeof (1 != 2)); // Prediction: Boolean
+console.log(typeof ('hamburger' + 's')); // Prediction: String
+console.log(typeof ('hamburgers' - 's')); // Prediction: NaN
+console.log(typeof ('1' + '3')); // Prediction: String
+console.log(typeof ('1' - '3')); // Prediction: NaN
+console.log(typeof ('johnny' + 5)); // Prediction: String
+console.log(typeof ('johnny' - 5)); // Prediction: NaN
+console.log(typeof (99 * 'luftbaloons')); // Prediction: NaN
