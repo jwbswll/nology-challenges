47f42fb740a46a63d1b72826c9077c31435e45b9
jwbswll <boswelljackwilliam@gmail.com>
2023-06-05 10:07:36 +1000

    changed my answers to js types challenge

--- js-challenges/types-challenge.js
+++ js-challenges/types-challenge.js
@@ -5,9 +5,9 @@ console.log(typeof 'hello'); // Prediction: String
 console.log(typeof true); // Prediction: Boolean
 console.log(typeof (1 != 2)); // Prediction: Boolean
 console.log(typeof ('hamburger' + 's')); // Prediction: String
-console.log(typeof ('hamburgers' - 's')); // Prediction: NaN
+console.log(typeof ('hamburgers' - 's')); // Prediction: Number
 console.log(typeof ('1' + '3')); // Prediction: String
-console.log(typeof ('1' - '3')); // Prediction: NaN
+console.log(typeof ('1' - '3')); // Prediction: Number
 console.log(typeof ('johnny' + 5)); // Prediction: String
-console.log(typeof ('johnny' - 5)); // Prediction: NaN
-console.log(typeof (99 * 'luftbaloons')); // Prediction: NaN
+console.log(typeof ('johnny' - 5)); // Prediction: Number
+console.log(typeof (99 * 'luftbaloons')); // Prediction: Number
