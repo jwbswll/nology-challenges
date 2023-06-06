f40d6257bbc6c7ad7fcc3e2d1ba61aabfcbd8cc7
jwbswll <boswelljackwilliam@gmail.com>
2023-06-06 13:04:25 +1000

    adds more control flow challenges

--- js-challenges/control-flow-challenge.js
+++ js-challenges/control-flow-challenge.js
@@ -1,3 +1,4 @@
+// challenge 1
 let x = 3;
 let y = 4;
 
@@ -9,7 +10,8 @@ if (x > y) {
   console.log("x is equal to y")
 }
 
-let userInput = 'hello'
+// challenge 2
+let userInput = 2
 
 if (parseInt(userInput)) {
   console.log(userInput**2)
@@ -18,3 +20,67 @@ if (parseInt(userInput)) {
 } else {
   console.log("Invalid Input")
 }
+
+// challenge 3
+let dayNumber = 8
+
+switch(dayNumber) {
+  case 1:
+    console.log("Today is Monday");
+    break;
+  case 2:
+    console.log("Today is Tuesday");
+    break;
+  case 3: 
+    console.log("Today is Wednesday");
+    break;
+  case 4:
+    console.log("Today is Thursday");
+    break;
+  case 5: 
+    console.log("Today is Friday");
+    break;
+  case 6: 
+    console.log("Today is Saturday");
+    break;
+  case 7:
+    console.log("Today is Sunday");
+    break;
+  default:
+    console.log("Input is invalid");
+}
+
+// challenge 4
+let parent1 = "green";
+let parent2 = "green";
+let blue = 0;
+let brown = 0;
+let green = 0;
+
+if (parent1 === "blue" && parent2 === "blue") {
+  blue = 99;
+  brown = 0;
+  green = 1;
+} else if (parent1 === "brown" && parent2 === "brown") {
+  blue = 6.25;
+  brown = 75;
+  green = 18.75;
+} else if (parent1 === "green" && parent2 === "brown" || parent1 === "brown" && parent2 === "green") {
+  blue = 12.5;
+  brown = 50;
+  green = 37.5;
+} else if (parent1 === "blue" && parent2 === "brown" || parent1 === "brown" && parent2 === "blue") {
+    blue = 50;
+    brown = 50;
+    green = 0;
+} else if (parent1 === "green" && parent2 === "green") {
+    blue = 25;
+    brown = "<1";
+    green = 75;
+} else if (parent1 === "green" && parent2 === "blue" || parent1 === "blue" && parent2 === "green") {
+    blue = 50;
+    brown = 0;
+    green = 50;
+}
+
+console.log(`Your eye colours are ${parent1} and ${parent2}, the chances of your child having blue eyes is ${blue}%, brown is ${brown}% and green is ${green}%.`)
